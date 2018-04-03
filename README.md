10c62e33ff7e39eeac6b5fa8d1858112848c39c9
colg <121529654@qq.com>
2018-04-03 10:44:58 +0800

    更新部分配置

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # spring boot
-```
+---
 集成 Mybatis，通用Mapper，PageHelp分页插件， war包独立部署，Swagger2 API
-```
+
+# 开发建议
+---
+
+- 表名，建议使用小写，多个单词使用下划线拼接。 如：sys_user
+- 不是表中字段的属性必须加 @Transient 注解，建议使用dto
+- 开发规范建议遵循阿里巴巴java开发手册
+- 公司内部使用Swagger2来编写，管理api文档
+- 需要工具类的话建议先从 hutool 工具类中找，尽量精简项目
+- 日志，使用 private static final Log log = LogFactory.get(); 定义
+- 字段验证：直接调用Validator.isXXX(String value)既可验证字段，返回是否通过验证。如：Validator.isChinese("字段验证器");
+
+
+# 技术选型 & 文档
+---
+
+- [Spring Boot学习&使用指南](https://blog.csdn.net/NorRinInTheSky)
+- [MyBatis官方文档](http://www.mybatis.org/mybatis-3/zh/index.html)
+- [MyBatisb通用Mapper插件](https://mapperhelper.github.io/docs/)
+- [MyBatis PageHelper分页插件](https://pagehelper.github.io/)
+- [Fastjson](https://github.com/alibaba/fastjson/wiki/Quick-Start-CN)
+- [spring-boot-starter-dao](https://github.com/halober/spring-boot-starter-dao)
+- [spring-boot-starter-swagger](https://github.com/halober/spring-boot-starter-swagger)
+- [Hutool工具类参考文档](http://hutool.mydoc.io/)
+- 其他略
--- spring-boot-project-manager/src/main/java/com/github/manager/configurer/WebMvcConfigurer.java
+++ spring-boot-project-manager/src/main/java/com/github/manager/configurer/WebMvcConfigurer.java
@@ -2,8 +2,15 @@ package com.github.manager.configurer;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -12,54 +19,93 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+
 /**
  * Spring MVC 配置
  *
  * @author colg
  */
+@Configuration
 public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
 
+	private static final Log log = LogFactory.get();
+
 	/**
-	 * 使用FastJson解析json
+	 * 配置HttpMessageConverters用于读取或写入请求或响应的主体。
+	 * 
+	 * 使用FastJSON解析{@link com.alibaba.fastjson.JSON}
 	 */
 	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 		FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
 		FastJsonConfig config = new FastJsonConfig();
 		config.setSerializerFeatures(
-				SerializerFeature.WriteDateUseDateFormat,
-				SerializerFeature.WriteMapNullValue,
-				SerializerFeature.WriteNullListAsEmpty,
-				SerializerFeature.WriteNullStringAsEmpty,
-				SerializerFeature.DisableCircularReferenceDetect
+					SerializerFeature.WriteDateUseDateFormat,
+					SerializerFeature.WriteMapNullValue,
+					SerializerFeature.WriteNullListAsEmpty,
+					SerializerFeature.WriteNullStringAsEmpty,
+					SerializerFeature.DisableCircularReferenceDetect
 				);
 		converter.setFastJsonConfig(config);
 		converters.add(converter);
 	}
-	
+
 	/**
-	 * 统一异常处理 
+	 * 配置异常解析器。
 	 */
 	@Override
 	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
 		// TODO colg
+		log.info("统一异常处理");
 	}
-	
+
 	/**
 	 * 处理跨域问题
 	 */
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		// TODO colg
+		log.info("处理跨域问题");
 	}
-	
+
 	/**
-	 * 添加拦截器
-	 * 
-	 * @param registry
+	 * 添加Spring MVC生命周期拦截器，用于控制器方法调用的预处理和后处理。 拦截器可以被注册为适用于所有请求或被限制为一部分URL模式。
 	 */
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		// TODO colg
+		HandlerInterceptor interceptor = new HandlerInterceptor() {
+			
+			@Override
+			public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+					throws Exception {
+				// 在请求处理之前进行调用（Controller方法调用之前
+				log.info("进入拦截器");
+				return true;
+			}
+			
+			@Override
+			public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+					ModelAndView modelAndView) throws Exception {
+				// 请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）
+			}
+			
+			@Override
+			public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+					throws Exception {
+				// 在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行（主要是用于进行资源清理工作）
+			}
+		};
+		
+		registry.addInterceptor(interceptor);
+	}
+	
+	/**
+	 * 添加解析器以支持自定义控制器方法参数类型。
+	 */
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		log.info("参数解析器");
 	}
 }
